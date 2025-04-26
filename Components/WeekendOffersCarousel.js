cf48da477f6f6412b497eeff5ae5bb01d4722eae
Erick F. Castillo <erick.castillo@tellso.com>
2025-04-26 15:56:55 -0600

    Botones act 1

--- Components/WeekendOffersCarousel.js
+++ Components/WeekendOffersCarousel.js
@@ -118,7 +118,9 @@ const WeekendOffersCarousel = () => {
               onClick={handlePrev}
               className="absolute left-0 top-1/2 transform -translate-y-1/2 bg-white p-2 rounded-full shadow-md hover:bg-gray-100 text-black z-10"
             >
-              ◀
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
+              </svg>
             </button>
 
             {/* Flecha Derecha */}
@@ -126,8 +128,11 @@ const WeekendOffersCarousel = () => {
               onClick={handleNext}
               className="absolute right-0 top-1/2 transform -translate-y-1/2 bg-white p-2 rounded-full shadow-md hover:bg-gray-100 text-black z-10"
             >
-              ▶
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
+              </svg>
             </button>
+
           </div>
         </div>
       </div>
